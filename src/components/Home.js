da3fb4a905b0180d19e78dd1a4e4c56bb5b6e2dc
kim sangjae <bom0704@naver.com>
2023-07-04 17:53:13 +0900

    Update Home.js
    장바구니 아이콘

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,11 +5,11 @@ export default function Home() {
 
     useEffect(() => {
         document.title = "홈"
-    },[])
+    }, [])
 
     return (
         <>
-        <Scrollbtn/>
+            <Scrollbtn />
             <div className="flex mx-4">
                 <img
                     src="https://www.ikea.com/images/be/24/be241105e72cf8bfb1fcd875f03827fe.jpg?f=xl"
@@ -128,6 +128,17 @@ export default function Home() {
                         <div className="absolute top-0 left-0 bg-[#fa0764] text-white px-1"><span className="text-xs">베스트셀러</span></div>
                         <p className="font-semibold mt-2">KLEPPSTAD 클렙스타드</p>
                         <p className="font-semibold mt-2 text-2xl">￦18900</p>
+                        <div>
+                            <svg
+                                className="fill-white"
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="16" height="16"
+                                viewBox="0 0 16 16">
+                                <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z" />
+                                <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
+                            </svg>
+                        </div>
+
                     </div>
                     <div className="relative mr-4">
                         <img
