8c105f12d02a578c263c701f146d9f0cc1909132
parkheeyeun <bom0704@naver.com>
2023-07-06 17:15:38 +0900

    Update Cart.js
    ㅠ

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
-import Count from "./Count";
 
 export default function Cart() {
+
+    const [count, setCount] = useState(0)
 
     const [cart, setCart] = useState(JSON.parse(localStorage.getItem("cart") || "[]"))
 
@@ -15,7 +16,6 @@ export default function Cart() {
                 <p className="text-4xl font-semibold flex justify-center items-center">No Item</p>
             )}
 
-
             {cart.map(item => (
                 <li className="list-none text-xl p-4 border-b border-black m-4" key={item.id}>
                     <img className="w-[200px] float-left mr-4" src={item.src} />
@@ -23,7 +23,14 @@ export default function Cart() {
                         <p className="font-semibold">{item.title}</p>
                         <p className="text-lg font-semibold mt-2">{item.price}</p>
                         <div className="mt-2">
-                           <Count/>
+                            {/* <p className="text-lg">{item.count}</p> */}
+                            <p className="text-lg">{count}</p>
+                            <button
+                                className="border rounded-full bg-blue-500 px-3 py-1 mr-2 text-white"
+                                onClick={(count +1)}>+1</button>
+                            <button
+                                className="border rounded-full bg-blue-500 px-3 py-1 mr-2 text-white"
+                                onClick={() => setCount(count - 1)}>-1</button>
                         </div>
                     </div>
                 </li>
